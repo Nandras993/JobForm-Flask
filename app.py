b06fec437bab10ba8643b51d83c2bcc93472c47c
Nandras993 <n.andras993@gmail.com>
2023-04-02 13:06:59 +0200

    Added email sending with message, added config file,

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from datetime import datetime
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+import json
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "myflaskapp"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"  # mysql:///data.db if you use mysql
+path = "config.json"  # Rename config-example.json to config.json and fill it with your data
+with open(path, "r") as handler:
+    info = json.load(handler)
+
+app.config["SECRET_KEY"] = info["SECRET_KEY"]
+app.config["SQLALCHEMY_DATABASE_URI"] = info["SQLALCHEMY_DATABASE_URI"]  # mysql:///data.db if you use mysql
+app.config["MAIL_SERVER"] = info["MAIL_SERVER"]
+app.config["MAIL_PORT"] = info["MAIL_PORT"]
+app.config["MAIL_USE_SSL"] = info["MAIL_USE_SSL"]
+app.config["MAIL_USERNAME"] = info["MAIL_USERNAME"]
+app.config["MAIL_PASSWORD"] = info["MAIL_PASSWORD"]
+
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -35,6 +49,17 @@ def index():
                     occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body = f"Thank you for your submission, {first_name}! " \
+                       f"Here are your data:\n{first_name}\n{last_name}\n{date}\n{email}\n{occupation}\n" \
+                       f"We will contact you soon!"
+        message = Message(subject="New job application form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+
+        mail.send(message)
+
         flash(f"Dear {first_name}, your form was submitted successfully!", "success")
 
     return render_template("index.html")
